e4c4a5bfc22feef322ae3e47a66dad2b012e1f18
Daniel Arellano <dkarella@usc.edu>
2015-11-24 16:29:20 -0800

    Testing Jenkins CI Server...

--- tests/sample-test.js
+++ tests/sample-test.js
@@ -1,8 +1,8 @@
 describe('Sample Test', function(){
     it('should multiply', function(){
         var a = 10;
-        var b = 23;
+        var b = 24;
         var res = a*b;
-        expect(res).toEqual(230);
+        expect(res).toEqual(240);
     })
 });
