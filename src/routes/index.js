e91beb3ee6eab96c38d37a32a938666fe0701fc2
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2015-11-30 16:01:13 -0800

    Added basic mongodb functionality. Users saved to a database

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,43 @@
 var router = require('express').Router(),
-    Users = require('../Models/user.js');
-
+    User = require('../Models/user.js');
 
 router.get('/employees', function(req, res){
-   console.log(Users.getEmployee());
-   res.json(Users.getEmployee());
+
+	//Basic user being saved to mongodb. 
+	var user = User({ 
+		netid: 'testemp',
+        empid: '123456',
+        firstname: 'Test',
+        lastname: 'Employee',
+        region: '987430',
+        supervisors: [],
+        timesheets: [
+            {
+                start: new Date('1/1/16'),
+                end: new Date('1/15/16'),
+                total_hours: 80,
+                timesheet_item: {
+                    date: new Date('1/1/16'),
+                    organization: 'USC ITS',
+                    punch_in: new Date('1/1/16T09:00:00'),
+                    punch_out: new Date('1/1/16T17:00:00')
+                }
+            }
+        ]});
+
+	user.save(function(err){
+    	if(err){
+    		return res.send(err);
+    	}
+    	else{
+    		console.log('Saved');
+    	}
+    });
+
+	User.find(function (err, users) {
+	  if (err) return console.error(err);
+	  res.json(users);
+	});
 });
 
 module.exports = router;
