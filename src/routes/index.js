4cb31c519661631826fc434450c31bf72c99eac8
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2015-12-02 14:35:23 -0800

    Added route for query of mongodb by netid and empid

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,43 +1,14 @@
 var router = require('express').Router(),
     User = require('../Models/user.js');
 
-router.get('/employees', function(req, res){
+router.get('/', function(req, res){
 
-	//Basic user being saved to mongodb. 
-	var user = User({ 
-		netid: 'testemp',
-        empid: '123456',
-        firstname: 'Test',
-        lastname: 'Employee',
-        region: '987430',
-        supervisors: [],
-        timesheets: [
-            {
-                start: new Date('1/1/16'),
-                end: new Date('1/15/16'),
-                total_hours: 80,
-                timesheet_item: {
-                    date: new Date('1/1/16'),
-                    organization: 'USC ITS',
-                    punch_in: new Date('1/1/16T09:00:00'),
-                    punch_out: new Date('1/1/16T17:00:00')
-                }
-            }
-        ]});
-
-	user.save(function(err){
-    	if(err){
-    		return res.send(err);
-    	}
-    	else{
-    		console.log('Saved');
-    	}
-    });
-
-	User.find(function (err, users) {
+    //This route will be passed a user's id and employee id within the query. Mongo will be queried and the results displayed
+    User.find({ netid: req.query.netid, empid: req.query.empid}, function (err, users) {
 	  if (err) return console.error(err);
 	  res.json(users);
 	});
+
 });
 
 module.exports = router;
