2148935d7db41d52c173e7ef6afbac0861cd2f1f
Heidi Negron <heidi.negron1@upr.edu>
2015-12-08 16:38:09 -0800

    css updates

--- src/public/modules/history_report/historycontroller.js
+++ src/public/modules/history_report/historycontroller.js
@@ -3,7 +3,7 @@
     //Record example
     var payRecord1 = { payPeriod: "06/03/2015" , regular: "59.3", overtime: "0.0", doubletime: "0.0", vac:"3.8", sick:"3.5", otherpaid:"8.3", otherunpaid:"0.0", totalHours: "74.9", status:"Approved"};
     var payRecord2 = { payPeriod: "05/20/2015", regular: "66.8", overtime: "0.0", doubletime: "0.0", vac: "3.8", sick: "4.5", otherpaid: "8.3", otherunpaid: "0.0", totalHours: "74.1", status: "Approved" };
-    $scope.week = 7;
+
     //Get Employee
     $scope.employee = { name: "Hasdf Aasdklf", region: "2510251000", id: "555", position:"Program Assistant"}
 
