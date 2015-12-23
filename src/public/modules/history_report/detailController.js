dd27ebc257de7511c9a0a2903fd05f914aec91a4
Daniel Arellano <arellanodanielkeith@gmail.com>
2015-12-23 13:06:24 -0800

    Changed Date formatting

--- src/public/modules/history_report/detailController.js
+++ src/public/modules/history_report/detailController.js
@@ -1,9 +1,10 @@
-angular.module('app').controller('detailController', ['$scope', '$location', '$routeParams' , '$rootScope', 'DataLoader', function ($scope, $location, $routeParams, $rootScope, DataLoader) {
+angular.module('app').controller('detailController', ['$scope', '$location', '$routeParams', '$filter', 'DataLoader', function ($scope, $location, $routeParams, $filter, DataLoader) {
 
     DataLoader.loadIfNeeded(function(err){
         if(err) console.log(err);
         else{
-            $scope.timesheet = $rootScope.employee.timesheets[$routeParams.timesheet-1];
+            $scope.timesheet = $filter('orderBy')($scope.employee.timesheets, '-end')[$routeParams.timesheet - 1];
+            console.log($scope.timesheet);
         }
     });
 
